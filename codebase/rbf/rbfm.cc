a3fc1a253bf9561ba829563b14f8c1e558fe9560
jbalasin <jbalasin@ucsc.edu>
2016-04-22 22:26:17 -0700

    Update rbfm.cc

--- codebase/rbf/rbfm.cc
+++ codebase/rbf/rbfm.cc
@@ -62,9 +62,77 @@ RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
 	return i;
 }
 
-RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const void *data, RID &rid) {
-    return -1;
+int RecordBasedFileManager::get_space_we_need(const vector<Attribute> &recordDescriptor, const void *data) {
+
+	unsigned vc = 0;
+
+	for (auto i = 0; i < recordDescriptor.size(); i++)
+		if (recordDescriptor[i].type == 0)
+		{
+			PI.space_we_need += 4;
+		}
+		else if (recordDescriptor[i].type == 1) {
+			PI.space_we_need += 4;
+		}
+		else if (recordDescriptor[i].type == 2) {
+			memcpy(&vc, (char*)data + PI.space_we_need, 4);
+			
+			PI.space_we_need += 4;
+			PI.space_we_need += vc;
+		}
+				
+	return PI.space_we_need;
 }
+
+bool RecordBasedFileManager::endOfPage() {
+	if (PAGE_SIZE - PI.where_to_start_writing - PI.space_we_need > 0) {
+		return false;
+	}
+	return true;
+}
+
+RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const void *data, RID &rid) {
+	PagedFileManager PFM = PagedFileManager();
+	FILE * F = fileHandle.getFile();
+	void * page_space = malloc(PAGE_SIZE);
+
+	PI.space_we_need = get_space_we_need(recordDescriptor,data);
+
+	if (PI.pages == 0) {
+		rid.pageNum = 1;
+		rid.slotNum = 1;
+		PI.pages++;
+		PI.slot = 1;
+		PI.where_to_start_writing += PI.space_we_need;
+
+		memcpy(((char*) page_space + PI.where_to_start_writing), data, PI.space_we_need);
+		fileHandle.writePage(1, page_space);
+	}
+	else {
+		if (endOfPage())
+		{
+			PI.pages++;
+			PI.slot = 1;
+			PI.where_to_start_writing += PI.space_we_need;
+
+			memcpy(((char*)page_space + PI.where_to_start_writing), data, PI.space_we_need);
+			fileHandle.appendPage(page_space);
+			// create new page // 
+		}
+		else {
+			rid.pageNum = PI.pages;
+			rid.slotNum = PI.slot++;
+			memcpy(((char*)page_space + PI.where_to_start_writing), data, PI.space_we_need);
+			fileHandle.writePage(PI.pages, page_space);
+
+		}
+	}
+	
+	
+
+	return -1;
+}
+
 
 RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const vector<Attribute> &recordDescriptor, const RID &rid, void *data) {
     return -1;
