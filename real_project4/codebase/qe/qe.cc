7d1138f6f49f59fa1425175249f82013daedad71
Ryan Nicholas Brounley <rbrounle@unix3.lt.ucsc.edu>
2016-06-01 23:34:03 -0700

    skeleton code

--- real_project4/codebase/qe/qe.cc
+++ real_project4/codebase/qe/qe.cc
@@ -4,4 +4,35 @@
 Filter::Filter(Iterator* input, const Condition &condition) {
 }
 
-// ... the rest of your implementations go here
+~Filter::Filter(){}
+
+RC Filter::getNextTuple(void *data){}
+
+void Filter::getAttributes(vector<Attribute> &attrs)
+const{}
+
+
+Project::Project(Iterator *input, const vector<string> &attrNames){}
+
+~Project::Project(){}
+
+int Project::getNextTuple(void *data){}
+
+// For attribute in vector<Attribute>, name it as rel.attr
+void Project::getAttributes(vector<Attribute> &attrs) 
+const{}
+
+
+INLJoin::INLJoin(Iterator *leftIn,
+		 IndexScan *rightIn,
+		 const Condition &condition){}
+
+~INLJoin::INLJoin(){}
+
+RC INLJoin::getNextTuple(void *data){}
+
+// For attribute in vector<Attribute>, name it as rel.attr
+void getAttributes(vector<Attribute> &attrs)
+const{
+
+}
