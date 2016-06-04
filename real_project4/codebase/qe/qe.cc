4998e9bb5b01065a552104486506ce8a84b55f37
Ryan Nicholas Brounley <rbrounle@unix1.lt.ucsc.edu>
2016-06-04 15:09:18 -0700

    commited changes

--- real_project4/codebase/qe/qe.cc
+++ real_project4/codebase/qe/qe.cc
@@ -169,48 +169,4 @@ void Filter::getAttributes(vector<Attribute> &attrs) const{
     ItF->getAttributes(attrs);
 }
 
-Project::Project(Iterator *input, const vector<string> &attrNames){
-    this->pIt = input;
-    pAttrs = attrNames;
 
-}
-
-
-RC Project::getNextTuple(void *data){
-   // pIt->getNextTuple(data);
-   return 0; 
-}
-
-// For attribute in vector<Attribute>, name it as rel.attr
-void Project::getAttributes(vector<Attribute> &attrs) const{
-    attrs.clear(); // clear the passed in vector
-
-    // get attributes from iterator field and store them in a vector
-    vector<Attribute> iterAttrs;
-    pIt->getAttributes(iterAttrs);
-
-    // nested for loops to compare each item 
-    // to project with all elements in a column
-    for(auto i = 0; i < pAttrs.size(); i++) {
-        for(auto j = 0; j < iterAttrs.size(); j++) {
-            if(pAttrs[j].compare(iterAttrs[j].name) == 0){
-                attrs.push_back(iterAttrs[j]);
-            }
-            break;
-        }
-    }
-
-}
-
-
-INLJoin::INLJoin(Iterator *leftIn,
-		 IndexScan *rightIn,
-		 const Condition &condition){}
-
-
-RC INLJoin::getNextTuple(void *data){}
-
-// For attribute in vector<Attribute>, name it as rel.attr
-void INLJoin::getAttributes(vector<Attribute> &attrs) const{
-
-}
